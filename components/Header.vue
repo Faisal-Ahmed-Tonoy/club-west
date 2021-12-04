b7f70f42114ceec0c45fa1ef5643ba991ab78c0f
unknown <faisalahmedtonoy@gmail.com>
2021-12-04 07:21:58 +0600

    text & logo

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,21 @@
 <template>
  
-<header class="bg-white shadow">
-    
-        <div class="banner-img w-full bg-cover bg-center" >
-         <div class="banner-img flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50" style="height:191px; background-image: url('/image/banner.png');">
-         <div class="text"></div>
-         </div>
-    </div>
+<header>
+     
+  <div class="bg-cover bg-no-repeat bg-center"  style="height:191px; background-image: url('/image/banner.png'); ">
+   
+ <div class="text-left">
+       <img src="~assets/image/logo.png" alt="">        <span>  <h1  class="text-white text-2xl font-semibold uppercase ml-32 md:text-3xl"> The  </h1></span>   
+       <img src="~assets/image/logo.png" alt="">  
+                 <h1 class="text-white text-2xl font-semibold uppercase md:text-3xl">Club West </h1>
+                
+               
+            </div>
+   
+  </div>
+  
+ 
+  
    
 
     

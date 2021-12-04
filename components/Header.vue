ef78f828ff2b624a11e515ebb61fca91891399ef
unknown <faisalahmedtonoy@gmail.com>
2021-12-04 06:12:21 +0600

    banner image

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+<header class="bg-white shadow">
+    
+        <div class="banner-img w-full bg-cover bg-center" >
+         <div class="banner-img flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50" style="height:191px; background-image: url('/image/banner.png');">
+         <div class="text"></div>
+         </div>
+    </div>
+   
+
+    
+</header>
+ 
+
+
+
+</template>

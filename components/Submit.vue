ee87f398b827fa474eba0b0665378fa8a36eb6bc
unknown <faisalahmedtonoy@gmail.com>
2021-12-15 03:32:32 +0600

    input field first draft

--- components/Submit.vue
+++ components/Submit.vue
@@ -1,24 +1,40 @@
 <template>
-  <div class="grid grid-cols-12 gap-4  ">
-    <div class="col-span-7   mt-24 ml-48 mb-28 text-left">
-      <div>
-        <p
-          class="text-middleBlue  text-justify   font-bold text-5xl "
-        >
-          JOIN THE WAITLIST
-        </p>
+  <div class="grid grid-cols-12 gap-4">
+    <div class="col-span-7 mt-24 ml-48 mb-28 text-left">
+      <div class="h-28">
+        <p class="text-middleBlue font-bold text-5xl">JOIN THE WAITLIST</p>
       </div>
       <div>
-        <p class="text-indigoDye    text-2xl"> 
-          Register below to be among the first to learn about leasing
-          opportunities once we open.
+        <p class="text-indigoDye text-2xl -mt-8">
+          Register below to be among the first to learn about 
+          <br />
+          leasing opportunities once we open.
         </p>
-      </div>
+      </div> 
+      
+        <form class="mt-8">
+          <div>
+           
+            	<input class=" font-bold text-middleBlue text-3xl  w-full placeholder-middleBlue value-middleBlue border-b border-middleBlue  focus:outline-none " type="" placeholder="*   First Name" >
+          </div>
+          <div>
+           
+            	<input class=" font-bold text-middleBlue text-3xl  w-full placeholder-middleBlue value-middleBlue border-b border-middleBlue  focus:outline-none " type="" placeholder="*   First Name" >
+          </div>
+          <div>
+           
+            	<input class=" font-bold text-middleBlue text-3xl  w-full placeholder-middleBlue value-middleBlue border-b border-middleBlue  focus:outline-none " type="" placeholder="*   First Name" >
+          </div>
+          <div>
+           
+            	<input class=" font-bold text-middleBlue text-3xl  w-full placeholder-middleBlue value-middleBlue border-b border-middleBlue  focus:outline-none " type="" placeholder="*   First Name" >
+          </div>
+        </form>
+      
     </div>
 
     <div class="col-span-5 flex border-4 justify-center items-center">
-      
-      <img class="" src="~assets/image/submit.png" alt="" /> 
+      <img class="" src="~assets/image/submit.png" alt="" />
     </div>
   </div>
 </template>

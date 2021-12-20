fe91496601500e9eb500b04a57554380e677f86b
unknown <faisalahmedtonoy@gmail.com>
2021-12-21 05:38:57 +0600

    responsive header section formatted

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,4 +26,22 @@
       </div>
     </div>
   </footer> -->
+  <div class="xl:pl-36 xl:pr-48 md:px-3" style=" background-image: url('/image/banner.png')  ">
+   <div class="container  ">
+      <div class="row ">
+         <div class="md:grid grid-cols-1   ">
+            <div class="md:flex justify-between ">
+               <div class="md:h-28 md:w-96 md:py-10 xm:py-10 "> 
+                  <img class="    " src="~assets/image/logo.png" alt="" />  
+               </div>
+               <div class="md:mt-20  md:mb-16">
+                  <button class="bg-treasuredTeal md:w-56 w-full h-16 ">
+                     <h1 class="text-white font-bold">Register</h1>
+                  </button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
 </template>

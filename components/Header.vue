0513bfdff5a9d3f9e1661b3907752252bbc46d6f
unknown <faisalahmedtonoy@gmail.com>
2021-12-21 04:57:13 +0600

    desktop version second draft

--- components/Header.vue
+++ components/Header.vue
@@ -1,53 +1,5 @@
 <template>
-  <!-- <div 
-    
-    >
-      <div class="flex justify-between">
-        <img class="ml-32 my-8 h-32" src="~assets/image/logo.png" alt="" />
-
-        <div class="mr-44 mt-20 mb-16">
-          <button class="bg-treasuredTeal w-56 h-16">
-            <h1 class="text-white font-bold">Register</h1>
-          </button>
-        </div> 
-      </div>
-    </div> -->
-  <!-- <div class="bg-indigo-500 lg:h-48  ">   
-  <div   class="py-10 h-full  lg:justify-between sm:items-center  sm:justify-center bg-cover bg-no-repeat bg-center sm:flex-row lg:flex md:flex-none"
-      style="background-image: url('/image/banner.png')" >
-      <div class="md:w-96 md:h-28 md:ml-32">
-          <img class="   " src="~assets/image/logo.png" alt="" /> 
-      </div>
-      <div class="lg:mr-44 flex flex-row md:items-center justify-center md:pt-10">
-      <button class="bg-treasuredTeal w-56 h-16    ">
-            <h1 class="text-white font-bold">Register</h1>
-          </button>
-        
-      </div>
-    
- 
-  </div>
-</div> -->
-  <!-- <div class="container min-w-full ">
-  
-      <div
-        class="py-10 h-full lg:justify-between sm:items-center sm:justify-center bg-cover bg-no-repeat bg-center sm:flex-row lg:flex md:flex-none"
-        style="background-image: url('/image/banner.png')"
-      >
-        <div class="md:w-96 md:h-28 md:ml-32  pb-4">
-          <img class="  md:ml-20  " src="~assets/image/logo.png" alt="" />
-        </div>
-        <div
-          class="lg:mr-44 flex flex-row md:items-center justify-center md:pt-10 "
-        >
-          <button class="bg-treasuredTeal w-56 h-16 ">
-            <h1 class="text-white font-bold">Register</h1>
-          </button>
-        </div>
-      </div>
-  
-  </div> -->
-  <div class="lg:pl-36 lg:pr-48 md:px-3" style=" background-image: url('/image/banner.png')  "> 
+   <div class="xl:pl-36 xl:pr-48 md:px-3" style=" background-image: url('/image/banner.png')  "> 
     <div class="container  "> 
         <div class="row ">
            

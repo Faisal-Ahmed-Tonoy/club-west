0710817f4f0a7a5e87bb1445a44b2738f1b4257d
unknown <faisalahmedtonoy@gmail.com>
2021-12-22 07:09:18 +0600

    header and footer mobile and desktop draft

--- components/Footer.vue
+++ components/Footer.vue
@@ -26,22 +26,23 @@
       </div>
     </div>
   </footer> -->
-  <div style="background-image: url('/image/banner.png');">
+<div style="background-image: url('/image/banner.png');">
     <div class="grid grid-cols-1 md:grid-cols-3">
-      <div class="flex items-center justify-center mt-5">
+      <div class="flex items-center justify-center mt-5 lg:mb-14 lg:mt-28"> 
         <img class=" " src="~assets/image/footerText.png" alt="" />
       </div>
-      <div class="text-white flex flex-col items-center justify-center mt-5">
-        <p class="font-bold">845-735-3155</p>
-        <p class="xm:mx-4 sm:mx-0">
+      <div class="text-white flex flex-col items-center justify-center mt-5"> 
+        <p class="font-bold lg:text-3xl">845-735-3155</p>
+        <p class="xm:mx-4 sm:mx-0 md:text-center font-bold lg:text-2xl">
           1000 Murray Court (Located off West Blue Hill Road) | Pearl River, NY
           10965
         </p>
       </div>
       <div class="flex flex-row items-center justify-center mt-5 pb-5">
-        <img src="~assets/image/houselogo.png" class="h-9 px-9" alt="" />
-        <img src="~assets/image/chairlogo.png" class="h-9 px-9" alt="" />
+        <img src="~assets/image/houselogo.png" class=" mx-3" alt="" />
+        <img src="~assets/image/chairlogo.png" class="  mx-3" alt="" />
         <p class="text-sm text-white">Site by: paceadv.com</p>
+       
       </div>
     </div>
   </div>
--- components/Header.vue
+++ components/Header.vue
@@ -5,10 +5,10 @@
   >
     <div class="container">
       <div class="row">
-        <div class="md:grid grid-cols-1  xm:px-2 xl:pl-36 xl:pr-48 ">
+        <div class="md:grid grid-cols-1  xm:px-2 xl:pl-36 xl:pr-48  ">
           <div class="md:flex justify-between">
             <div class="md:h-28 md:w-96 md:py-10 xm:py-10">
-              <img class="    " src="~assets/image/logo.png" alt="" />
+              <img class="    " src="~assets/image/logo.png" alt="" />  
             </div>
             <div class="md:mt-20 md:mb-16">
               <button class="bg-treasuredTeal md:w-56 w-full h-16">

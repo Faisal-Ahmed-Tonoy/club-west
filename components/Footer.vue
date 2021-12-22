6cee242e5e33766ecb8416a32a0b3f7380fb6448
unknown <faisalahmedtonoy@gmail.com>
2021-12-22 23:54:37 +0600

    removing footer comment

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,31 +1,5 @@
 <template>
-  <!-- <footer>
-    <div
-      class="grid grid-cols-3 gap-4 bg-cover bg-no-repeat bg-center"
-      style="height: 191.68px; background-image: url('/image/banner.png')"
-    >
-      <div >
-        <img
-          class="ml-32 mt-24 mb-8"
-          src="~assets/image/footerText.png"
-          alt=""
-        />
-      </div>
-
-      <div class="text-white text-center">
-        <p class="font-bold mt-14 pb-3 text-3xl">845-735-3155</p>
-        <p class="text-2xl ">
-          1000 Murray Court (Located off West Blue Hill Road) | Pearl River, NY
-          10965
-        </p>
-      </div>
-      <div :style class="flex justify-evenly text-white ml-32 mt-24 mb-8">
-        <img src="~assets/image/houselogo.png" class="h-9" alt="" />
-        <img src="~assets/image/chairlogo.png" class="h-9" alt="" />
-        <p class="font-medium">Site by: paceadv.com</p>
-      </div>
-    </div>
-  </footer> -->
+ 
 <div style="background-image: url('/image/banner.png');">
     <div class="grid grid-cols-1 md:grid-cols-3">
       <div class="flex items-center justify-center mt-5 lg:mb-14 lg:mt-28"> 

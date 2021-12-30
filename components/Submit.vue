666d30d486975cdfd2cbf6a92b412f3dc37b1b10
unknown <faisalahmedtonoy@gmail.com>
2021-12-31 02:15:41 +0600

    sumit ok

--- components/Submit.vue
+++ components/Submit.vue
@@ -1,154 +1,60 @@
 <template>
-  <!-- <div class="grid grid-cols-12 gap-4 h-1015">
-    <div class="col-span-7 mt-24 ml-48 mb-28 text-left">
-      <div class="h-28">
-        <p class="text-middleBlue font-bold text-5xl">JOIN THE WAITLIST</p>
-      </div>
-      <div>
-        <p class="text-indigoDye text-2xl -mt-8">
-          Register below to be among the first to learn about
-          <br />
-          leasing opportunities once we open.
-        </p>
-      </div>
-
-      <form class="mt-8">
-        <div>
-          <input
-            class="font-bold text-middleBlue text-3xl text-upper w-full placeholder-middleBlue value-middleBlue border-b border-middleBlue focus:outline-none focus:bg-white"
-            type=""
-            placeholder="*         First Name"
-          />
-        </div>
-        <div class="pt-8">
-          <input
-            class="font-bold text-indigoDye text-3xl w-full placeholder-indigoDye value-middleBlue border-b border-middleBlue focus:outline-none"
-            type=""
-            placeholder="*         Last Name"
-          />
-        </div>
-        <div class="pt-8">
-          <input
-            class="font-bold text-indigoDye text-3xl w-full placeholder-indigoDye value-middleBlue border-b border-middleBlue focus:outline-none"
-            type=""
-            placeholder="*         Email"
-          />
-        </div>
-        <div class="pt-8">
-          <input
-            class="font-bold text-indigoDye text-3xl w-full placeholder-indigoDye value-middleBlue border-b border-middleBlue focus:outline-none"
-            type=""
-            placeholder="*         Phone"
-          />
-        </div>
-        <div class="pt-8">
-          <input
-            class="font-bold text-indigoDye text-3xl w-full placeholder-indigoDye value-middleBlue border-b border-middleBlue focus:outline-none"
-            type=""
-            placeholder="     Comments"
-          />
-        </div>
-        <div class="pt-6">
-          <input
-            class="text-indigoDye text-1xl w-full placeholder-indigoDye value-middleBlue focus:outline-none"
-            type=""
-            placeholder="*                Required fields"
-          />
-        </div>
-        <button class="bg-treasuredTeal w-96 h-20 mt-12">
-          <h1 class="text-white font-bold">Submit</h1>
-        </button>
-      </form>
-    </div>
-
-    <div class="col-span-5 flex justify-center items-center">
-      <img class="w-679 h-709" src="~assets/image/submit.png" alt="" />
-    </div>
-  </div> -->
-  <!-- <div class="grid grid-cols-1 gap-4 lg:grid-cols-12 mt-28"> 
-    <div class="lg:col-span-7">
-      <div class="flex flex-col xl:items-start md:ml-20  xl:ml-52  xm:ml-8 ">  
-         
-           <p class="text-middleBlue  font-bold md:ml-20 xl:ml-52 text-5xl  my-2 "> 
-        JOIN THE WAITLIST
-      </p>
-      <p class="text-indigoDye  xl:text-2xl text-xl md:ml-20 xl:ml-52  "> Register below to be among the first to learn about <br> leasing opportunities once we open. </p>
   
- <form>
-
-  <input type="text" id="firstName" name="firstName" value="John"><br>
-  
-  <input type="text" id="lastName" name="lastName" value="Doe"><br><br>
- <button type="submit">Submit</button>
-</form> 
-          
-      </div>
-     
-    </div>
-    <div class="lg:col-span-5 md:mt-5"> 
-     
-         <img class="border-solid   m-auto  lg:pr-32 " src="~assets/image/submit.png" alt="" />  
-     
-    </div>
-  </div> -->
-  <div class="grid lg:grid-cols-12 lg:gap-4 xm:mt-28">
+  <div class="grid lg:grid-cols-12 lg:gap-4 mt-28  ">
     <form
       action=""
-      class="lg:col-span-7 border-solid border-4 border-red-500 xm:mb-28 xm:px-6 lg:ml-20" 
+      class="lg:col-span-7    xm:px-6 lg:ml-20"
     >
-      <p class="text-middleBlue font-bold text-5xl xm:my-8">
+      <p class="text-middleBlue font-bold text-5xl xm:mb-8">
         JOIN THE WAITLIST
       </p>
-      <p class="xm:my-8 text-indigoDye text-2xl  xm:w-560 lg:w-512">
+      <p class="xm:my-8 text-indigoDye  text-2xl  xm:w-560 lg:w-512">
         Register below to be among the first to learn about leasing
         opportunities once we open.
       </p>
-       <div class="star font-bold text-middleBlue text-3xl xm:my-8    border-b border-middleBlue  ">
-    <label class="md:ml-14 "> </label> 
-    <input class="font-bold text-middleBlue placeholder-middleBlue text-3xl  w-5/6 uppercase focus:outline-black  " type="text"  placeholder="First Name" required>
+       <div class="star font-bold text-middleBlue text-3xl py-2 xm:my-10    border-b border-middleBlue  ">
+    <label class="md:ml-12 "> </label> 
+    <input class="font-bold text-middleBlue placeholder-middleBlue text-3xl  w-5/6 uppercase focus:outline-none  " type="text"  placeholder="First Name" required>
     
 </div>
-  <div class="star font-bold text-middleBlue text-3xl xm:my-8   placeholder-middleBlue  border-b border-middleBlue  ">
-    <label class="md:ml-14 "> </label> 
+  <div class="star font-bold text-middleBlue text-3xl py-2  xm:my-10  placeholder-middleBlue  border-b border-middleBlue  ">
+    <label class="md:ml-12 "> </label> 
     <input class="font-bold text-indigoDye placeholder-indigoDye  text-3xl w-5/6 uppercase  focus:outline-none  " type="text"  placeholder="Last Name" required>
     
 </div>
-  <div class="star font-bold text-middleBlue text-3xl xm:my-8    placeholder-middleBlue  border-b border-middleBlue  ">
-    <label class="md:ml-14 "> </label> 
+  <div class="star font-bold text-middleBlue text-3xl py-2  xm:my-10  placeholder-middleBlue  border-b border-middleBlue  ">
+    <label class="md:ml-12 "> </label> 
     <input class="font-bold text-indigoDye placeholder-indigoDye  text-3xl  w-5/6 uppercase focus:outline-none  " type="text"  placeholder="Email" required>
     
 </div>
-  <div class="star font-bold text-middleBlue text-3xl xm:my-8     border-b border-middleBlue  ">
-    <label class="md:ml-14 "> </label> 
+  <div class="star font-bold text-middleBlue text-3xl py-2 xm:my-10   border-b border-middleBlue  ">
+    <label class="md:ml-12 "> </label> 
     <input class="font-bold text-indigoDye placeholder-indigoDye  text-3xl  w-5/6 uppercase focus:outline-none  " type="text"  placeholder="Phone" required>
     
 </div>
-<div class=" font-bold text-middleBlue text-3xl xm:my-8   border-b border-middleBlue  ">
-    <label class="md:ml-14 "> </label> 
-    <input class="font-bold text-indigoDye placeholder-indigoDye  text-3xl  w-5/6 uppercase focus:outline-none  " type="text"  placeholder="  Comment" required> 
+<div class=" font-bold text-middleBlue text-3xl py-2  xm:my-10   border-b border-middleBlue  ">
+    <label class="md:ml-8 "> </label> 
+    <input class="font-bold text-indigoDye placeholder-indigoDye  text-3xl  w-5/6 uppercase focus:outline-none  " type="text"  placeholder="Comment" required> 
     
 </div>
-<div class="star  text-indigoDye xm:my-8       ">
-    <p class="inline pl-10">Required Field </p>
+<div class="star  text-middleBlue xm:my-8">
+    <p class="text-indigoDye  inline pl-5"> Required Field </p>
     
 </div>
-<div class="xm:w-full border-4 border-red-500 xm:my-8">
-     <button class=" border-4 border-red-500 bg-treasuredTeal py-2 lg:w-2/4 w-full   h-16">
+<div class="xm:w-full   xm:my-8">
+     <button class="bg-treasuredTeal py-2 lg:w-2/4 w-full   h-16"> 
                 <h1 class="text-white font-bold">Submit</h1>
               </button>
 </div>
     </form>
 
-    <div class="border-4 border-red-500 lg:col-span-5">
-      <img class="md:w-full p-2" src="~assets/image/submit.png" alt="" />
+     <div class="   lg:col-span-5  lg:mr-32  lg:my-auto ">
+      <img class="w-full  xl:my-24 "  src="~assets/image/submit.png" alt="" /> 
     </div>
   </div>
 </template>
  <style>
     .star::before { 
       content:"*";
-      
-
-    
-    }
+}
 </style>

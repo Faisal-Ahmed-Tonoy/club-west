3b88f3f3f6484efb358187f8e993039c1171c6e5
unknown <faisalahmedtonoy@gmail.com>
2022-01-06 01:03:08 +0600

    main section removing comment

--- components/Main.vue
+++ components/Main.vue
@@ -1,34 +1,5 @@
 <template>
-  <!-- <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-2 ">
-    <div class="flex flex-col items-center justify-center lg:mt-20 lg:ml-20 lg:mr-14 ">
-      <p class="text-indigoDye font-bold text-5xl text-center my-2">
-        ULTRA-LUXURY
-        <br />
-        RENTALS FOR 55+
-      </p>
-      <img class="my-2" src="~assets/image/Line15.png" alt="" />
-      <p class="text-middleBlue font-bold text-5xl text-center">
-        Opening Spring 2022
-      </p>
-      <p class="my-2 text-center text-indigoDye text-2xl"> 
-        An encore to The Club at Pearl River,
-        <br />
-        The Club West will introduce a brand-new
-        <br />
-        collection of one and two-bedroom rental
-        <br />
-        residences surrounded by a wealth of
-        <br />
-        lifestyle amenities in scenics
-        <br />
-        Pearl River, New York.
-      </p>
-      <img class="my-2" src="~assets/image/Polygon3.png" alt="" />
-    </div>
-    <div class="lg:col-span-2">
-      <img class="lg:w-full lg:h-full" src="~assets/image/kitchen.png" alt="" />
-    </div>
-  </div> -->
+   
    <div class="grid xl:grid-cols-3 xl:gap-2">
      <div class="xl:w- xl:col-span-1 lg:mt-24 px-8 my-8  "> 
          <p class="xl:text-center text-justify text-indigoDye font-bold text-5xl xl:mb-12 mb-8  "> 

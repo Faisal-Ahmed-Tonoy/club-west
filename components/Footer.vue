0e0a402f52f576138fad187b56988d650937bb1e
unknown <faisalahmedtonoy@gmail.com>
2022-01-07 04:45:35 +0600

    footer done

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,32 +1,13 @@
 <template>
  
-<!-- <div class="lg:mt-28 mt-8" style="background-image: url('/image/banner.png');"> 
-    <div class="grid grid-cols-1 lg:grid-cols-3">
-      <div class="flex items-center justify-center mt-5 lg:mb-14 lg:mt-28"> 
-        <img class=" " src="~assets/image/footerText.png" alt="" />
-      </div>
-      <div class="text-white flex flex-col items-center justify-center mt-5"> 
-        <p class="font-bold lg:text-3xl">845-735-3155</p>
-        <p class="xm:mx-4 sm:mx-0 md:text-center font-bold lg:text-2xl">
-          1000 Murray Court (Located off West Blue Hill Road) | Pearl River, NY
-          10965
-        </p>
-      </div>
-      <div class="flex flex-row items-center justify-center mt-5 pb-5"> 
-        <img src="~assets/image/houselogo.png" class=" mx-3" alt="" />
-        <img src="~assets/image/chairlogo.png" class="  mx-3" alt="" />
-        <p class="text-sm text-white">Site by: paceadv.com</p>
-       
-      </div>
-    </div>
-  </div> -->
+ 
   <footer>
     <div class="grid lg:grid-cols-8 gap-10 px-8 " style="background-image: url('/image/banner.png');" > 
         <div class="grid lg:col-span-2   xl:ml-28  mx-auto my-8 lg:mt-28 lg:mb-14 ">  
 <img class=" " src="~assets/image/footerText.png" alt="" />
     </div>
      <div class="grid lg:col-span-4      "> 
-  <p class="lg:mt-12   lg:border-solid    mx-auto mb-8 font-bold text-3xl text-white text-justify">845-735-3155</p>
+  <p class="lg:mt-12   lg:border-solid     mb-8 font-bold text-3xl text-white  text-center">845-735-3155</p>
   <p class="lg:mb-10 border-solid   mx-auto text-2xl text-white  "> 
           1000 Murray Court (Located off West Blue Hill Road) | Pearl River, NY
           10965
@@ -34,11 +15,11 @@
     </div>
      <div class="grid  lg:col-span-2   "> 
 
- <div class="flex items-center justify-center   xm:space-x-6 my-8 lg:mt-28   ">
+ <div class="flex items-center justify-center    my-8 lg:mt-28   ">
 <div>
   <img src="~assets/image/houselogo.png" class=" " alt="" />
 </div>
-        <div class=" ">
+        <div class="px-10">
            <img src="~assets/image/chairlogo.png" class="   " alt="" />    
         </div>
         <p class="text-sm text-white">Site by: paceadv.com</p>

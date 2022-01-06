581555abc3e1e2b696374611137dc858d6c0bb6e
unknown <faisalahmedtonoy@gmail.com>
2022-01-06 06:41:58 +0600

    submit section text-justify

--- components/List.vue
+++ components/List.vue
@@ -1,55 +1,14 @@
 <template>
-  <!-- <div class="grid grid-cols-1 lg:grid-cols-3 lg:gap-10 mt-28">
-    <div class="lg:col-span-2">
-      <img class="lg:w-full lg:h-full" src="~assets/image/Group67.png" alt="" />
-    </div>
-    <div>
-      <div
-        class="flex flex-col items-center justify-center lg:items-start lg:justify-start"
-      >
-        <p
-          class="text-indigoDye font-bold sm:text-5xl xm:text-5xl mb-8 lg:mt-20 xm:ml-10"
-        >
-          IT’S YOUR TIME
-          <br />
-          TO EMBRACE
-          <br />
-          THE GOOD LIFE
-        </p>
-
-        <ul
-          class="list-disc list-inside text-indigoDye sm:text-2xl xm:text-xl xm:ml-10"
-        >
-          <li>Private gated community</li>
-          <li>Modern 1- & 2-bedroom residences</li>
-          <li>Den & private outdoor space available</li>
-          <li>Clubhouse with bar & resident lounge</li>
-          <li>Fitness center & yoga studio</li>
-          <li>Library & coffee bar</li>
-          <li>Poker & card rooms</li>
-          <li>Shuffleboard</li>
-          <li>Outdoor swimming pool</li>
-          <li>Outdoor bar & TV</li>
-          <li>Firepit</li>
-          <li>Private BBQ & dining areas</li>
-          <li>Pickleball court</li>
-          <li>Dog run</li>
-          <li>Amazon package lockers</li>
-          <li>24-hour onsite maintenance</li>
-        </ul>
-        <img class="my-2 lg:ml-28" src="~assets/image/Polygon3.png" alt="" />
-      </div>
-    </div>
-  </div> -->
+   
   <div class="grid lg:grid-cols-3 lg:gap-10">
-    <div class="lg:col-span-1 px-6 lg:mt-36  "> 
+    <div class="lg:col-span-1 px-8 lg:mt-36  "> 
        <p class="  text-indigoDye font-bold text-5xl mb-12   "> 
         IT’S YOUR TIME 
 TO EMBRACE 
 THE GOOD LIFE
        </p>
       <ul
-        class=" px-6 mb-8 list-disc  list-outside  text-indigoDye text-2xl lg:leading-10 lg:tracking-wider  "
+        class=" px-6 mb-8 list-disc  list-outside  text-indigoDye text-2xl lg:leading-10 lg:tracking-wider  " 
       >
         <li>Private gated community</li>
         <li>Modern 1- & 2-bedroom residences</li>
